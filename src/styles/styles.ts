fd031a480f561c4ee4c015f97abc54a878ec5fb3
mateus <smsilva.mateus@gmail.com>
2023-03-07 07:04:56 -0300

    initial structure style

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -56,6 +56,14 @@ export const OtherOptionsWrapper = styled.div`
 `
 
 export const WrapperSchedule = styled.div`
+  background: #fbfbfb;
+  border-radius: 8px;
+  box-shadow: 1px 2px 8px rgba(0, 0, 0, 0.65);
+  margin: 2rem auto 8.1rem auto;
+  width: 329px;
+  padding: 10px;
+
   display: flex;
   flex-direction: column;
+  gap: 3px;
 `
